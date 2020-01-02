5b291e27f42e70fb5270eb51924ae99bccfa3a98
SD170 <duttasaswata7@gmail.com>
2020-01-03 01:08:55 +0530

    Updates

---
+++ precache-manifest.da14144a229ddee27142e23b7283581d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e576270a5b9dc5731dad1bf13c4100",
+    "url": "/SoccerStars/public/index.html"
+  },
+  {
+    "revision": "6290d48363ee89cc765c",
+    "url": "/SoccerStars/public/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4bd03f52949814709ce1",
+    "url": "/SoccerStars/public/static/css/main.ea5f6d3e.chunk.css"
+  },
+  {
+    "revision": "6290d48363ee89cc765c",
+    "url": "/SoccerStars/public/static/js/2.1ba80ad8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SoccerStars/public/static/js/2.1ba80ad8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bd03f52949814709ce1",
+    "url": "/SoccerStars/public/static/js/main.96918434.chunk.js"
+  },
+  {
+    "revision": "a69c4ee006b597e63e15",
+    "url": "/SoccerStars/public/static/js/runtime-main.5c30a8b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SoccerStars/precache-manifest.df76167595fe51d2cf70dc8fe3e1af18.js"
+  "/SoccerStars/public/precache-manifest.da14144a229ddee27142e23b7283581d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SoccerStars/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SoccerStars/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
